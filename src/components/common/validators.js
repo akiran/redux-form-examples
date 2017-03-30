acb29a015d739481e3ad945f2f945b0297c249be
kiran <kiran@neostack.com>
2017-03-30 09:12:02 +0530

    email validation

--- src/components/common/validators.js
+++ src/components/common/validators.js
@@ -1,3 +1,9 @@
+import isEmail from 'validator/lib/isEmail';
+
 export function required(value) {
   return value ? undefined : 'Required'
+}
+
+export function email(value) {
+  return value && isEmail(value) ? undefined : 'Not a valid email'
 }
--- src/components/forms/ValidationForm.js
+++ src/components/forms/ValidationForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {RaisedButton} from 'material-ui'
 import {reduxForm, Field} from 'redux-form'
 import {TextField} from 'redux-form-material-ui'
-import {required} from '../common/validators'
+import {required, email} from '../common/validators'
 
 function minLength(value) {
   return value && value.length > 3 ? undefined : 'username should be ateast 3 characters long'
@@ -21,12 +21,22 @@ export default class ValidationForm extends React.Component {
       <div>
         <h1>Validation form</h1>
         <form onSubmit={handleSubmit}>
-          <Field
-            name='username'
-            placeholder='User name'
-            component={TextField}
-            validate={[required, minLength]}
-          />
+          <div>
+            <Field
+              name='username'
+              placeholder='User name'
+              component={TextField}
+              validate={[required, minLength]}
+            />
+          </div>
+          <div>
+            <Field
+              name='email'
+              placeholder='E-mail'
+              component={TextField}
+              validate={email}
+            />
+          </div>
           <div>
             <RaisedButton type='submit' label="Submit" primary={true} />
           </div>
